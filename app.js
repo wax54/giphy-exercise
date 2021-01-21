7ef26099e864a647a721279ab9b775f235cd49fa
sam <wax654@gmail.com>
2021-01-20 18:16:31 -0800

    got one image!

--- app.js
+++ app.js
@@ -1,14 +1,37 @@
 $('#search-form').on('submit', handleSubmit);
 console.log()
-const getSearchTerm = () => $('#query').value;
+const getSearchTerm = () => $('#query').val();
 
 function handleSubmit(evt) {
-    console.log(evt);
     evt.preventDefault();
     const searchTerm = getSearchTerm();
     displayNewGif(searchTerm);
 }
 
-function displayNewGif(searchQuery) {
-    console.log(searchQuery);
+async function displayNewGif(searchQuery) {
+    const res = await axios.get('https://api.giphy.com/v1/gifs/search',
+        {
+            params: {
+                q: searchQuery,
+                limit: 1,
+                api_key: 'MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym'
+
+            }
+        });
+    console.log(res.data.data[0].images.original.url);
+    addImageToBody(res.data.data[0].images.original.url);
+}
+
+
+function addImageToBody(imgUrl, id) {
+    const $img = newImage(imgUrl);
+    $img.attr('id', id);
+
+    $('body').append($img);
+}
+
+function newImage(src) {
+    const $img = $('<img class="img-thumbnail">');
+    $img.attr('src', src);
+    return $img;
 }
